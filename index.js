e0aff3e0959137f0a306d6962a873198628400e1
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2022-10-11 19:51:32 +0200

    get Text and href-URL using the enclosing tag's class id value

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 const axios = require('axios');
-//const cheerio = require('cheerio');
+const cheerio = require('cheerio');
 //const { response } = require('express');
 const express = require('express');
 const PORT = 8000;
 
 const app = express();
 
-const url_one = 'https://www.google.com/search?q=champions+league&oq=champions+league&aqs=chrome..69i57j69i61.5515j0j1&sourceid=chrome&ie=UTF-8#cobssid=s&sie=lg;/g/11mvmxlddg;2;/m/0c1q0;mt;fp;1;;;';
-
-
-function getCallback(req, res){
-	res.send('Your default "GET Request" gets this response right here!!')
-}
-
-function listen_atPortCallback(whateverPORT){
-	console.log(`My Port is listening at: ${whateverPORT}`);
-}
-
-app.get('/', getCallback);
+const url_one = 'https://www.cashcrusaders.co.za/';
 
 //Axios makes API call to URL. We get the response(HTML) and print it on the terminal. 
 //*chaining in Async JS
-/*
 axios(url_one)
 	.then(response => {
-		const html_frm_url = response.data
+		const html_frm_url = response.data;
 		//console.log(html_frm_url);
-	});
-*/
-//*With callback func
-//app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
 
-//without callback func
-app.listen(PORT, listen_atPortCallback(PORT));
+		const $ = cheerio.load(html_frm_url);
+		$('.col-1-3', html_frm_url).each(function(){
+			const compText = $(this).text();
+			const hrefVar = $(this).find('a').attr('href');
+			
+			console.log("URL Retrieved Is " + hrefVar);
+			console.log("Text Retrieved is " + compText);
+		});
+	});
+
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
