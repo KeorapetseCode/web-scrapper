1e1916c3c66ba7d2ba4b94f7f3ebbd0a5364da7e
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2022-10-11 19:21:58 +0200

    clearly defined callbacks

--- index.js
+++ index.js
@@ -6,16 +6,30 @@ const PORT = 8000;
 
 const app = express();
 
-const url_one = 'https://www.cashcrusaders.co.za/';
+const url_one = 'https://www.google.com/search?q=champions+league&oq=champions+league&aqs=chrome..69i57j69i61.5515j0j1&sourceid=chrome&ie=UTF-8#cobssid=s&sie=lg;/g/11mvmxlddg;2;/m/0c1q0;mt;fp;1;;;';
 
+
+function getCallback(req, res){
+	res.send('Your default "GET Request" gets this response right here!!')
+}
+
+function listen_atPortCallback(whateverPORT){
+	console.log(`My Port is listening at: ${whateverPORT}`);
+}
+
+app.get('/', getCallback);
 
 //Axios makes API call to URL. We get the response(HTML) and print it on the terminal. 
 //*chaining in Async JS
+/*
 axios(url_one)
 	.then(response => {
 		const html_frm_url = response.data
-		console.log(html_frm_url);
+		//console.log(html_frm_url);
 	});
+*/
+//*With callback func
+//app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
 
-//*There is a callback
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+//without callback func
+app.listen(PORT, listen_atPortCallback(PORT));
